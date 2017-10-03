637a856caa921253751797c6e209feb4ea0dbc72
John Mettraux <jmettraux@gmail.com>
2017-10-04 07:33:49 +0900

    Don't show images or iframes in index.html

--- lib/render_index.rb
+++ lib/render_index.rb
@@ -39,7 +39,7 @@ posts =
     content = content
       .split("\n", 2).last # remove title
       .split("\n")
-      .reject { |l| l.match(/^#/) }
+      .reject { |l| l.match(/^(#|<)/) }
     content = content[1, 2].join("\n") + "\n&hellip;"
     vars['CONTENT'] = Blog.md_render(content.substitute(vars), index: true)
 
