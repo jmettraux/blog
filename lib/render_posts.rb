d758bae5ebb9491e8b640ba889fe1293d0dd629f
John Mettraux <jmettraux@gmail.com>
2019-04-08 06:27:02 +0900

    Escape _ as &lowbar;

--- lib/render_posts.rb
+++ lib/render_posts.rb
@@ -35,6 +35,7 @@ Dir['posts/*.md'].sort.each do |path|
 
     vars, content = Blog.load_post(path)
 
+    content.gsub!('_', '&lowbar;')
     content.gsub!(/\n```([a-z]+)\s+/) { |m| "\n<pre><code class=\"#{m[4..-2]}\">" }
     content.gsub!(/\n```\n/, "\n</code></pre>\n")
 
